5871b744d6cc7be004446299f92ed9cca14d81b6
Cleggatron <david.robert.clegg@gmail.com>
2021-06-20 15:27:52 +0100

    added quiz questions and data tags for answers

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,6 +9,17 @@ var highScores = document.querySelector("#highscores");
 var activePage = startPage;
 var leaderboard = [];
 
+var questions =[
+    {question:"Q1: Data in an array is stored in...?", answer1: "Elements", answer2: "Compounds", answer3: "Solutions", answer4: "Boxes", actualAnswer: "answer1"},
+    {question:"Q2: If we want to apply the same style to a group of html elements we would use...?", answer1: "ID", answer2: "A Class", answer3: "A Variable", answer4: "The DOM", actualAnswer: "answer2"},
+    {question:"Q3: If we wanted to perform the same action multiple times we can use...?", answer1: "An Array", answer2: "An Object", answer3: "A Loop", answer4: "Logical Operators", actualAnswer: "answer3"},
+    {question:"Q4: The body tag is used to contain...?", answer1: "The content of the page", answer2: "The links to external CSS", answer3: "The value of any variables", answer4: "An If... Else statement", actualAnswer: "answer1"},
+    {question:"Q5: If we want to look at the code in the browers we right click and select...?", answer1: "Lookout", answer2: "Police", answer3: "Detecct", answer4: "Inspect", actualAnswer: "answer4"},
+    {question:"Q6: Which of the following is not a data type in Javascript?", answer1: "Object", answer2: "Long", answer3: "Undefined", answer4: "Number", actualAnswer: "answer2"},
+    {question:"Q7: Which company developed Javascript?", answer1: "Google", answer2: "Facebook", answer3: "Starbucks", answer4: "Netscape", actualAnswer: "answer4"},
+    {question:"Q8: How do we access an element of an array?", answer1: "arrayName.elementNumber", answer2: "arrayName[elementNumber]", answer3: "arrayName,elementNumber", answer4: "arrayName + elementNumber", actualAnswer: "answer2"},
+    {question:"Q9: What is the latest version of CSS?", answer1: "CSS1", answer2: "CSS4", answer3: "CSS2", answer4: "CSS3", actualAnswer: "answer3"},
+    {question:"Q10: <main>, <header>, and <footer> are examples of...?", answer1: "Semantic HTML elements", answer2: "Element IDs", answer3: "Inline elements", answer4: "Element classes", actualAnswer: "answer1"}];
 
 
 
@@ -30,7 +41,7 @@ function submitScore(initials, currentScore){
         name: initials.trim(),
         score: currentScore
     };
-    //check if we ahve anything in local storage
+    //check if we have anything in local storage
     if(localStorage.getItem("leaderboard") === null){
         leaderboard[0] = submission;
         localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
@@ -49,7 +60,7 @@ function submitScore(initials, currentScore){
 
 
 //print the leaderboard
-function updateScorecards(){
+function populateLeaderboard(){
     leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
     //print our leaderboard
     for(i = 0; i < leaderboard.length; i++){
